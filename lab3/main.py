1d2c1e2c76c2d31173e82370c067f54ebe449f9d
Raphael Ramos <raphaelramos@geowellex.com>
2024-10-24 15:13:56 -0300

    Group roles

--- lab3/main.py
+++ lab3/main.py
@@ -14,19 +14,27 @@
 #
 from pyspark.sql import SparkSession, Row
 
-spark = SparkSession \
-    .builder \
-    .appName("Lab 3") \
-    .getOrCreate()
+# Cria uma sessão Spark
+spark = SparkSession.builder.appName("Frequencia de cargos").getOrCreate()
 
+# Obtém o contexto Spark a partir da sessão Spark criada
 sc = spark.sparkContext
 
-lines = sc.textFile('conjunto2.csv')
+# Abre CSV e transforma cada linha em uma lista
+lines = sc.textFile('conjunto2.csv') \
+    .map(lambda l : l.split(';')) \
+    .filter(lambda l : l[0] != 'NOME' and not l[0].startswith('-') and len(l[1]) > 0)
+    # Filtra as linhas: 
+    # 1. Ignora a linha do cabeçalho.
+    # 2. Ignora as linhas onde o nome começa com um hífen.
+    # 3. Garante que a segunda coluna (ocupação) tenha conteúdo.
 
-columns = lines.map(lambda l : l.split(';'))
+# Mapeia as linhas filtradas para um RDD de Rows, contendo o nome da função e o nome da pessoa, e elimina duplicatas
+job_name = lines.map(lambda p: Row(job = p[1], name = p[0])).distinct()
 
-people = columns.map(lambda p: Row(name = p[0], job = p[1])).distinct()
+# Agrupa os nomes por função e conta quantas pessoas estão associadas a cada função
+role_count = job_name.groupByKey().mapValues(len)
 
-# Imprimir cinco primeiros resultados
-print("Sem repeticao")
-print(f'{people.count()} funcionarios')
+# Exibe a frequencia de cada cargo no sdout
+for role, count in sorted(role_count.collect()):
+    print(role, count)
